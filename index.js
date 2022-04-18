d6f701415e99e882906b86cc9d4d69493573bb02
SrConn0r <64154806+SrConn0r@users.noreply.github.com>
2022-04-17 20:00:24 -0500

    server in node, with socket and express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+// App express
+const app = express();
+
+// Node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+// path public
+const publicPath = path.resolve( __dirname, 'public');
+
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT , (err) =>{
+    if(err) throw new Error(err);
+    console.log('Server is running on port 3000!');
+});
---
+++ sockets/socket.js
@@ -0,0 +1,18 @@
+const {io} = require('../index');
+
+// Socket messages
+io.on('connection', client => {
+
+    console.log('Client connected');
+
+    client.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+
+    client.on('message', (payload) => {
+        console.log('Message received', payload);
+
+        io.emit('message', {admin: 'new message'});
+
+    });
+});
